900fe516da7227f3a3793182550346edf0570d95
giobero1 <giorgiberuchashvili312@gmail.com>
2024-10-18 21:49:21 +0400

    goa

---
+++ level 0/sdlkf.py
@@ -0,0 +1,12 @@
+saxeli = "gio"
+paroli = "gio123"
+a=input("sheiyvane saxeli")
+b=input("sheiyvane paroli")
+
+if a == saxeli and b == paroli:
+    print("sworia")
+    print(saxeli*100*10)
+else:
+    print("tavidan") 
+    
+
