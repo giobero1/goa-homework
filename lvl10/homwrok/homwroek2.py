8e02f09f9bd6d7bf53127a413d2117471d8a8e05
giobero1 <giorgiberuchashvili312@gmail.com>
2024-12-22 12:30:01 +0400

    Add files via upload

---
+++ lvl10/homwrok/homwroek2.py
@@ -0,0 +1,8 @@
+deda= int(input("sekvan deda  asaki"))
+mama =int(input("sekvane mama  asaki"))
+da1=int(input("sekvane  da1  asaki"))
+da2=int(input("sekvane da2 asaki"))
+
+dro= int(input("rramden xanshi gainteresebs?"))
+print("deda=",deda+dro,"mama=",mama+dro,"da1=",da1+dro,"da2",da2+dro)
+
