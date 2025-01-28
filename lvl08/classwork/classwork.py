18b9a677196a2ce7f11ebdf936cb313fcd86aba0
giobero1 <giorgiberuchashvili312@gmail.com>
2025-01-28 22:07:21 +0400

    Delete lvl08 directory

--- lvl08/classwork/classwork.py
+++
@@ -1,8 +0,0 @@
-saxeli=input("seikvane saxeli")
-print(saxeli)
-g=input("sheiyvane: pex")
-b=input("seiyvane: burti")
-print(g+b)
-
-#############3input aris raaaaac segvyavs compiuteri an 
-#klaviaturit an kamerit an mausis dacherit da output aris kompiuteridan gagamosuli inpormacia
