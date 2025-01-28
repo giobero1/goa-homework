fedd6d806f5bf4b3f10edf64e3a0025b0d4cfcbc
giobero1 <giorgiberuchashvili312@gmail.com>
2025-01-28 22:05:37 +0400

    Delete lvl10 directory

--- lvl10/homwrok/homework1.py
+++
@@ -1,5 +0,0 @@
-
-
-saheli= input("seikvane saheli")
-asaki=  int(input("sekvane   asaki"))
-print("gamarjoba",saheli,"! shen xar "  ,asaki,"wlis")
--- lvl10/homwrok/homework3.py
+++
@@ -1,5 +0,0 @@
-num1=  int(input("seikvane  ricxvi1:"))
-num2 =  int(input("seikkvane ricxvi2:"))
-
-
-print(num1+num2,num1-num2,num1*num2,num1/num2)
