17b880abc31565148e863442e179a720752c44e7
giobero1 <giorgiberuchashvili312@gmail.com>
2025-01-28 22:05:03 +0400

    first commit

---
+++ lvl12.5/classwork/classwork2.py
@@ -0,0 +1,4 @@
+name = input("Enter Your Name: ")
+num = int(input("Enter Your Number: "))
+
+print(name * num)
---
+++ lvl12.5/homework/homework2.py
@@ -0,0 +1,4 @@
+user = int(input("Enter Your Age: "))
+total = user + 40
+
+print("Your Age After 40 years:", total)v
